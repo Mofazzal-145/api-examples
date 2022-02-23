f32bea87b78a6250381afd937893f3664a7985b8
Mofazzal-145 <md.mofazzalhossain145@gmail.com>
2022-02-24 01:08:49 +0600

    country file

--- js/countries.js
+++ js/countries.js
@@ -14,14 +14,36 @@ const displayCountries = countries => {
     countries.forEach(country => {
         const div = document.createElement('div');
         div.classList.add('country');
-        const h3 = document.createElement('h3');
+        div.innerHTML = ` <h3> ${country.name}</h3>
+        <p>${country.capital}</p>
+        <button onclick="loadCountryByName('${country.name}')">Details</button>
+        `;
+        /* const h3 = document.createElement('h3');
         h3.innerText = `${country.name}`;
         div.appendChild(h3);
         const p = document.createElement('p');
         p.innerText = `${country.capital}`;
-        div.appendChild(p);
+        div.appendChild(p); */
         countriesDiv.appendChild(div);
         // console.log(country);
     })
 
+}
+
+const loadCountryByName = (name) => {
+    const url = `https://restcountries.com/v2/name/${name}`;
+    fetch(url)
+    .then(res => res.json())
+    .then(data => displayCountryDetail(data[0]))
+    // console.log(name);
+}
+
+const displayCountryDetail = country => {
+    console.log(country);
+    const countryDiv = document.getElementById('country-detail');
+    countryDiv.innerHTML = `
+    <h5>${country.name}</h5>
+    <p> population: ${country.population}</p>
+    <img width = "200px" src = "${country.flag}">
+    `;
 }
